1c176600598dc81138322e25fdba65f5a2c92317
Peter Haro <peter.haro@sintef.no>
2018-01-15 21:10:08 +0100

    Initial skeleton, creation of application with common filters, endpoints and other helpers. Showcase through the registered endpoints how to achieve alot of common functionality with flask. Register flask to load-balancers if available, or distributed cache(s).

---
+++ __main__.py
@@ -0,0 +1,11 @@
+import argparse
+
+import FlaskBase
+
+if __name__ == '__main__':
+    app = FlaskBase.app
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", type=int, default=5000)
+    parser.add_argument("--host", default="127.0.0.1")
+    args = parser.parse_args()
+    app.run(port=args.port, host=args.host)
